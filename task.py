ec95a99d1c108e159152047b31e31857558e1968
kirillcheck <32092781+kirillcheck@users.noreply.github.com>
2018-01-11 11:49:55 +0300

    Update task.py

--- task.py
+++ task.py
@@ -8,7 +8,7 @@ import pprint
 import json
 filename = 'data.json'
 try:
-    with open(filename, encoding='utf-8') as data_file:        
+    with open(filename, encoding='utf-8') as data_file:  #открывает файл с нужной нам кодировкой       
         data = json.loads(data_file.read())
 except OSError: # ошибка, связанная с системой.
     print("error 404. Или вы неправильно ввели имя")
